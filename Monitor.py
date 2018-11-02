57e81eb677c12efa67b65221c525ae6e74c741e6
Nawar Ismail <nawar@Nawars-MacBook-Air.local>
2018-11-02 18:53:47 -0400

    Added support for loose monitor cables

--- Monitor.py
+++ Monitor.py
@@ -5,6 +5,8 @@
 		1) 3 Dell U2141-H monitors are setup as follows:
 			[Monitor 1] - dp_out -> [Monitor 2] - dp_out -> [Monitor 3 (mst-off)]
 			Each has three inputs port: [dp, mdp, hdmi]
+		2) Devices are not limited to the number of monitors they can output to, or daisy-chain to.
+			This would only be an issue for older devices connecting to more than 3 monitors.
 
 	There are some definitions:
 		0) daisy-chaining: The act of passing the output of one monitor into another to display one device on multiple monitors.
@@ -21,6 +23,7 @@
 
 	Limitations:
 		1) Doesn't handle asking for nothing to be displayed on a monitor.
+		2) There are issues since the None's use the same output multiple times like laptop-hdmi-to-hdmi1 and laptop-hdmi-to-hdmi2
 
 	Improvements:
 		1) If there are loose cables (eg: a spare hdmi-hdmi connected to monitor 2) and the user wants to display 
@@ -40,7 +43,7 @@ class Cable:
 		self.output = output
 
 	def __repr__(self):
-		return self.input + " to " + self.output
+		return self.input + "-" + self.output
 
 class Device:
 	def __init__(self, name, outputs):
@@ -54,6 +57,18 @@ class Device:
 					return (True, cable)
 		return (False, None)
 
+	def possible_connections(self, cables):
+		monitor_inputs = []
+		for output in self.outputs:
+			monitor_inputs += [cable.output for cable in cables if output == cable.input]
+		return monitor_inputs
+
+	def how_does_connect_to(self, connected_to, cables):
+		monitor_inputs = []
+		for output in self.outputs:
+			monitor_inputs += [cable for cable in cables if output == cable.input and cable.output == connected_to]
+		return monitor_inputs
+
 class Connection:
 	def __init__(self, device, output, monitor, input, supports_daisy_chaining=False):
 		# could check to make sure this is valid given the cables.
@@ -63,25 +78,29 @@ class Connection:
 		self.monitor_input = input
 		self.supports_daisy_chaining = supports_daisy_chaining
 
+	def __repr__(self):
+		name = 'None' if not self.device else self.device.name
+		return name + " is connected from " + self.device_output + " to (" + str(self.monitor) + ")" + self.monitor_input
+
 cables = [
 	Cable('hdmi', 'hdmi'),
-	Cable('usb-c', 'dp'),
+	Cable('usbc', 'dp'),
 	Cable('mdp', 'dp'),
 ]
 
 devices = {
-	'desktop': Device('desktop', ['usb-c', 'hdmi']),  # Is there a way to avoid duplicate desktop? I guess a Devices class?
-	'laptop': Device('laptop', ['usb-c', 'hdmi']),
-	'mac': Device('mac', ['mdp']),
+	'desktop': Device('desktop', ['usbc', 'hdmi']),  # Is there a way to avoid duplicate desktop? I guess a Devices class?
+	'laptop': Device('laptop', ['usbc', 'hdmi']),
+	'mac': Device('mac', ['mdp', 'hdmi']), # hdmi just for testing
 }
 
 
 class Test:
 	@staticmethod
 	def device():
-		print(Device('desktop', ['usb-c', 'hdmi']).can_connect_to('hdmi', cables))
-		print(Device('desktop', ['usb-c', 'hdmi']).can_connect_to('mdp', cables))
-		print(Device('desktop', ['usb-c', 'hdmi']).can_connect_to('dp', cables))
+		print(Device('desktop', ['usbc', 'hdmi']).can_connect_to('hdmi', cables))
+		print(Device('desktop', ['usbc', 'hdmi']).can_connect_to('mdp', cables))
+		print(Device('desktop', ['usbc', 'hdmi']).can_connect_to('dp', cables))
 
 		print(Device('mac', ['mdp']).can_connect_to('hdmi', cables))
 		print(Device('mac', ['mdp']).can_connect_to('mdp', cables))
@@ -92,15 +111,13 @@ class Setup:
 	def __init__(self, number_of_monitors):
 		self.num_monitors = number_of_monitors
 		self.connections = [
-			Connection(devices['desktop'], 'usb-c', 1, 'dp', supports_daisy_chaining=True),
-			#Connection(devices['mac'], 'hdmi', 1, 'hdmi'),
-			Connection(devices['laptop'], 'usb-c', 2, 'dp', supports_daisy_chaining=True),
-			Connection(devices['laptop'], 'hdmi', 2, 'hdmi'),
+			Connection(devices['desktop'], 'usbc', 1, 'dp', supports_daisy_chaining=True),
+			Connection(devices['laptop'], 'usbc', 2, 'dp', supports_daisy_chaining=True),
 			Connection(devices['mac'], 'mdp', 3, 'dp'),
-			#Connection(None, 'hdmi', 1, 'hdmi'),  # Available hdmi-hdmi connection to monitor 3
-			#Connection(None, 'hdmi', 2, 'hdmi'),
-			#Connection(None, 'hdmi', 3, 'hdmi'),
-			#Connection(None, 'dpi', 1, 'mdp')
+			Connection(None, 'hdmi', 1, 'hdmi'),
+			Connection(None, 'hdmi', 2, 'hdmi'),
+			Connection(None, 'hdmi', 3, 'hdmi'),
+			Connection(None, 'dpi', 1, 'mdp')
 		]
 
 	def get_connection(self, monitor, input):
@@ -159,8 +176,6 @@ class Setup:
 					simplified_connections = []
 					for connection in connections:
 						simplified_connections.append([connection.monitor, connection.monitor_input])
-
-					
 					return all([smo in simplified_connections for smo in simplified_monitor_outputs])
 
 				valid = do_connections_support_configuration(connections, configuration)
@@ -173,18 +188,61 @@ class Setup:
 			devices_displayed = []
 			input_sources, monitor_inputs = monitor_output
 			for i, input in enumerate(monitor_inputs):
+				print(i, input)
 				for connection in self.connections:
 					if connection.monitor == input[0] and connection.monitor_input == input[1]:
-						devices_displayed.append(connection.device.name)
+						devices_displayed.append(None if not connection.device else connection.device.name)
+
+			if len(devices_displayed) > self.num_monitors:
+				msg = "You connected more than 1 device to the same input:\n"
+				for c in self.connections:
+					msg += '\t' + str(c) + '\n'
+				raise ValueError(msg)
 			return (input_sources, devices_displayed)
 			
 
 		def get_requested_inputs(request, possible_devices_displayed):
 			request = [r.lower() for r in request]
 			possible_inputs_for_request = []
-			for input_sources, devices in possible_devices_displayed:
-				if request == devices:
+
+			for input_sources, devices_displayed in possible_devices_displayed:
+				if request == devices_displayed:
 					possible_inputs_for_request.append(input_sources)
+			return possible_inputs_for_request
+
+		
+
+		def get_requested_inputs_via_new_connections(request, possible_devices_displayed):
+			# Make sure it doesn't tell you to connect two devices to the same monitor.
+			# This doesnt work, think about appending to a new list where None is replaced by all 
+			# possible valid connections, and calling the above code on that.
+			for p in possible_devices_displayed:
+				print(p)
+
+			request = [r.lower() for r in request]
+			new_possible_devices_displayed = []
+			for input_sources, devices_displayed in possible_devices_displayed:
+				if None in devices_displayed:
+					new_displays = (input_sources, [])
+					message = []
+					for i, device in enumerate(devices_displayed):
+						if device is None:
+							device_connects_to = devices[request[i]].possible_connections(cables)
+							if input_sources[i] in device_connects_to:
+								new_displays[1].append(request[i])
+								possible_cables = devices[request[i]].how_does_connect_to(input_sources[i], cables)
+								possible_cables = ' or '.join([str(c) for c in possible_cables])
+								message.append(request[i] + " to " + input_sources[i] + "("+str(i+1)+") by " + possible_cables)
+						else:
+							new_displays[1].append(device)
+					new_possible_devices_displayed.append((new_displays, ' and '.join(message)))
+
+			for p in new_possible_devices_displayed:
+				print('---',[p[0]], p[1])
+			possible_inputs_for_request = [(get_requested_inputs(request, [d[0]]), d[1]) for d in new_possible_devices_displayed]
+			possible_inputs_for_request = [p for p in possible_inputs_for_request if p[0]]
+			for p in possible_inputs_for_request:
+				print('>>>', p)
 			return possible_inputs_for_request
 
 		def summary_to_user(matching_input_sources):
@@ -207,7 +265,11 @@ class Setup:
 		possible_devices_displayed = [get_displayed_device(output) for output in possible_outputs]
 		matching_input_sources = get_requested_inputs(users_devices_request, possible_devices_displayed)
 		summary = summary_to_user(matching_input_sources)
-		# if you can't be displayed, see how they can use loose connections to get it like that.
+		if "no way" in summary:
+			summary = "You aren't able to get this configuration as is.\n"
+			summary += "However, by connecting loose cables:\n"
+			matching_input_sources = get_requested_inputs_via_new_connections(users_devices_request, possible_devices_displayed)
+			summary += summary_to_user(matching_input_sources)
 		return summary
 
 	def prompt_user(self):
@@ -232,7 +294,7 @@ if __name__ == '__main__':
 		#print("next")
 		#print(Setup(3).how_to_get(['Desktop', 'Laptop', None]))
 		#print("next")
-		print(Setup(3).how_to_get(['Mac', 'Laptop', 'Laptop']))
+		print(Setup(3).how_to_get(['Laptop', 'Laptop', 'Laptop']))
 		#print("next")
 		#print(Setup(3).how_to_get(['mac', None, None]))		
 		exit(0)
